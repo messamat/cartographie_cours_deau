61ad9698229731c846bc07182b2d046d8506cbed
messamat <messamat@uw.edu>
2023-11-09 18:34:35 -0500

    Restructure output path location, process BD forest, BDHaies, compute % irrigated area by commune, create points, format building data for dasymetric mapping of population

--- 9_preprocess_ancillary_data.py
+++ 9_preprocess_ancillary_data.py
@@ -4,17 +4,17 @@ from setup_classement import  *
 
 anci_dir = os.path.join(datdir, "données_auxiliaires")#Ancillary data directory
 
-
 #------------------------------------------- UNITS OF ANALYSIS ---------------------------------------------------------
 #Departments and communes - admin express
-admin_dir = os.path.join(anci_dir, "admin_express", "ADMIN-EXPRESS_3-2__SHP_WGS84G_FRA_2023-05-03",
-                    "1_DONNEES_LIVRAISON_2023-05-03", "ADE_3-2_SHP_WGS84G_FRA")
+admin_dir = os.path.join(anci_dir, "admin_express", "ADMIN-EXPRESS_3-2__SHP_LAMB93_FXX_2023-10-16",
+                         "ADMIN-EXPRESS", "1_DONNEES_LIVRAISON_2023-10-16", "ADE_3-2_SHP_LAMB93_FXX")
 deps = os.path.join(admin_dir, "DEPARTEMENT.shp")
 coms = os.path.join(admin_dir, "COMMUNE.shp")
 
 #---------------------------------------------- DATA SOURCES -----------------------------------------------------------
 #% of irrigated land by commune - agreste
-percent_irrigated_crops_communes = os.path.join(anci_dir, "agreste", "data.csv")
+irrigation_communes = os.path.join(anci_dir, "agreste", "data_irrigation_SAU_communes.csv")
+irrigation_departements = os.path.join(anci_dir, "agreste", "data_irrigation_SAU_departements.csv")
 
 #Barrier points - amber
 barriers = os.path.join(anci_dir, "amber", "AMBER_BARRIER_ATLAS_V1.csv")
@@ -24,28 +24,23 @@ fish_stations = os.path.join(anci_dir, "aspe", "raw_data", "raw_data", "csv", "s
 
 #DEM - BDAlti
 bdalti_dir = os.path.join(anci_dir, "bdalti")
-bdalti_filelist = getfilelist(bdalti_dir, "^BDALTIV2_25M_FXX_.*[.]asc$")
 
 #Lithology
 bdcharm_dir = os.path.join(anci_dir, "bdcharm50")
-bdcharm_filelist = getfilelist(bdcharm_dir,"^GEO050K_HARM_[0-9]{3}_S_FGEOL_2154[.]shp$")
 
 #Forest type
 bdforet_dir = os.path.join(anci_dir, "bdforet_v2")
-bdforet_filelist = getfilelist(bdforet_dir, "FORMATION_VEGETALE.shp")
 
 #Dominant soil type 1:1,000,000
 bdgsf = os.path.join(anci_dir, "bdgsf", "30169_L93", "30169_L93.shp")
 
 #BD topo 2023
 bdtopo2023_dir = os.path.join(anci_dir, "bdtopo233") #233 is the version/package number (vs e.g. 151 for 2015)
-bdtopo2023_gpkglist = getfilelist(bdtopo2023_dir, "BDT_3-3_GPKG_LAMB93_R[0-9]{2}-ED2023-09-15.gpkg$")
-bdtopo2023_haie_filelist = [os.path.join("main.haie") for gpkg in bdtopo2023_gpkglist]
 "plan_d_eau"
 "reservoir"
 
 #Withdrawal points - bnpe
-withdrawal_pts=os.path.join(anci_dir, "bnpe", "bnpe_ouvrages.csv")
+withdrawal_stations_bnpe=os.path.join(anci_dir, "bnpe", "bnpe_ouvrages.csv")
 
 #GlobalSoilModel - available water capacity ############### TO FINISH
 gsm_awc_dir = os.path.join(anci_dir, "gsm")
@@ -56,8 +51,13 @@ farm_parcels = os.path.join(anci_dir, "ign_rpg", "RPG_2-0__GPKG_LAMB93_FXX_2022-
                             "PARCELLES_GRAPHIQUES.gpkg", "main.parcelles_graphiques")
 #print(arcpy.GetCount_management(farm_parcels)[0])
 
+#Buildings
+bdtopo2019_dir = os.path.join(anci_dir, 'bdtopo191')
+
 #Population count - insee
-pop_count = os.path.join(anci_dir, "insee_pop", "carreaux_nivNaturel_met.shp")
+pop_count_variable_mesh = os.path.join(anci_dir, "insee_pop", "carreaux_nivNaturel_met.shp")
+pop_count_200m_mesh = os.path.join(anci_dir, "insee_pop", "arreaux_200m_shp")
+
 
 #Hydrobiology sampling stations - naiade
 hydrobio_stations = os.path.join(anci_dir,"naiade", "stations.csv")
@@ -73,21 +73,45 @@ lc_filedict = {yr: getfilelist(os.path.join(lc_dir, "oso_{}".format(yr)),"Classi
 #Snelder intermittence
 snelder_ires = os.path.join(anci_dir, "snelder", "rhtvs2_all_phi_qclass.shp")
 
+#-------------------------------- OUTPUTS ------------------------------------------------------------------------------
 #Preprocessing geodatabase
 pregdb = os.path.join(resdir, "preprocessing_ancillary_data.gdb")
 if not arcpy.Exists(pregdb):
     arcpy.CreateFileGDB_management(out_folder_path=os.path.split(pregdb)[0],
                                    out_name=os.path.split(pregdb)[1])
 
+#Scratch gdb
+tempgdb = os.path.join(resdir, "scratch.gdb")
+if not arcpy.Exists(tempgdb):
+    arcpy.CreateFileGDB_management(out_folder_path=os.path.split(tempgdb)[0],
+                                   out_name=os.path.split(tempgdb)[1])
+
+#Template sr
+sr_template = arcpy.Describe(coms).SpatialReference
+
+#Output layers
+bdalti_mosaic = os.path.join(pregdb, "bdalti_25m_mosaic")
+bdalti_slope = os.path.join(pregdb, "bdalti_25m_slope")
+bdalti_tcurvature = os.path.join(pregdb, "bdalti_25m_curvature_tangential")
+bdalti_pcurvature = os.path.join(pregdb, "bdalti_25m_curvature_profile")
+bdcharm_fr = os.path.join(pregdb, "lithology_bdcharm_fr")
+bdforet_fr = os.path.join(pregdb, "bdforet_fr")
+bdhaies_fr = os.path.join(pregdb, "bdhaies_fr")
+irrig_coms_formatted = os.path.join(resdir, "agreste_irrig_deps_formatted.csv")
+irrig_coms_poly = os.path.join(resdir, "communes_irrig")
+barriers_pts_proj = os.path.join(pregdb, 'barriers_amber_proj')
+fish_stations_pts = os.path.join(pregdb, 'fish_stations_aspe')
+withdrawal_pts_proj = os.path.join(pregdb, "withdrawals_bnpe_proj")
+buildings_filtered_fr = os.path.join(pregdb, "buildings_filtered_fr")
 
 #--------------------------------- DEM - BDALTI ------------------------------------------------------------------------
-bdalti_mosaic = os.path.join(pregdb, "bdalti_25m_mosaic")
 #Based on metadata from https://geoservices.ign.fr/documentation/donnees/alti/bdalti
 #https://geoservices.ign.fr/sites/default/files/2021-07/DC_BDALTI_2-0.pdf
 bdalti_sr = arcpy.SpatialReference(5698)
 
 if not arcpy.Exists(bdalti_mosaic):
     print("Processing {}...".format(bdalti_mosaic))
+    bdalti_filelist = getfilelist(bdalti_dir, "^BDALTIV2_25M_FXX_.*[.]asc$")
     arcpy.MosaicToNewRaster_management(input_rasters=bdalti_filelist,
                                        output_location=os.path.split(bdalti_mosaic)[0],
                                        raster_dataset_name_with_extension=os.path.split(bdalti_mosaic)[1],
@@ -97,13 +121,11 @@ if not arcpy.Exists(bdalti_mosaic):
                                        )
 
 #Compute slope
-bdalti_slope = os.path.join(pregdb, "bdalti_25m_slope")
 if not arcpy.Exists(bdalti_slope):
     print("Processing {}...".format(bdalti_slope))
     Slope(in_raster=bdalti_mosaic, output_measurement="DEGREE").save(bdalti_slope)
 
 #Compute curvature
-bdalti_tcurvature = os.path.join(pregdb, "bdalti_25m_curvature_tangential")
 if not arcpy.Exists(bdalti_tcurvature):
     print("Processing {}...".format(bdalti_tcurvature))
     SurfaceParameters(in_raster=bdalti_mosaic,
@@ -112,7 +134,6 @@ if not arcpy.Exists(bdalti_tcurvature):
                       z_unit='METER'
                       ).save(bdalti_tcurvature)
 
-bdalti_pcurvature = os.path.join(pregdb, "bdalti_25m_curvature_profile")
 if not arcpy.Exists(bdalti_pcurvature):
     print("Processing {}...".format(bdalti_pcurvature))
     SurfaceParameters(in_raster=bdalti_mosaic,
@@ -123,26 +144,200 @@ if not arcpy.Exists(bdalti_pcurvature):
 
 #Compute topographic wetness index in the future - https://mapscaping.com/topographic-wetness-index-in-arcgis-pro/
 
-#--------------------------------- Lithology - BDCHARM ------------------------------------------------------------------------
-{}
+#--------------------------------- Lithology - BDCHARM -----------------------------------------------------------------
+if not arcpy.Exists(bdcharm_fr):
+    print("Processing {}...".format(bdcharm_fr))
+    bdcharm_filelist = getfilelist(bdcharm_dir, "^GEO050K_HARM_[0-9]{3}_S_FGEOL_2154[.]shp$")
+    arcpy.management.Merge(inputs=bdcharm_filelist, output=bdcharm_fr)
 
-bdcharm_mosaic = os.path.join(pregdb, "lithology_bdcharm_mosaic")
-bdalti_sr = arcpy.SpatialReference(5698)
+#--------------------------------- Forest type - BD foret  -------------------------------------------------------------
+if not arcpy.Exists(bdforet_fr):
+    print("Processing {}...".format(bdforet_fr))
+    bdforet_filelist = getfilelist(bdforet_dir, "FORMATION_VEGETALE.shp$")
+    arcpy.management.Merge(inputs=bdforet_filelist, output=bdforet_fr)
 
-if not arcpy.Exists(bcae_fr):
-    arcpy.management.Merge(inputs=bcae_filelist, output=bcae_fr)
+#--------------------------------- Hedges - BDHaies  -------------------------------------------------------------------
+if not arcpy.Exists(bdhaies_fr):
+    print("Processing {}...".format(bdhaies_fr))
+    bdtopo2023_gpkglist = getfilelist(bdtopo2023_dir, "BDT_3-3_GPKG_LAMB93_R[0-9]{2}-ED2023-09-15.gpkg$")
+    bdhaies_filelist = [os.path.join(gpkg, "main.haie") for gpkg in bdtopo2023_gpkglist]
+    arcpy.management.Merge(inputs=bdhaies_filelist, output=bdhaies_fr)
 
-if not arcpy.Exists(bdalti_mosaic):
-    print("Processing {}...".format(bdalti_mosaic))
-    arcpy.MosaicToNewRaster_management(input_rasters=bdcharm_filelist,
-                                       output_location=os.path.split(bdalti_mosaic)[0],
-                                       raster_dataset_name_with_extension=os.path.split(bdalti_mosaic)[1],
-                                       coordinate_system_for_the_raster=bdalti_sr,
-                                       pixel_type="16_BIT_SIGNED",
-                                       number_of_bands=1
-                                       )
+#--------------------------------- Get % irrigated area by commune -----------------------------------------------------
+if not arcpy.Exists(irrig_coms_poly):
+    #Read tables
+    irrig_deps_pd = pd.read_csv(filepath_or_buffer=irrigation_departements, sep=";", skiprows=2)
+    irrig_coms_pd = pd.read_csv(filepath_or_buffer=irrigation_communes, sep=";", skiprows=2)
 
+    # Format column names
+    irrig_deps_pd.columns = ["{}_dep".format(
+        re.sub("[*=,()]", "",
+               re.sub("(\\s[-]\\s)|\\s", "_", col)))
+        for col in irrig_deps_pd.columns]
+    irrig_coms_pd.columns = ["{}_com".format(
+        re.sub("[*=,()]", "",
+               re.sub("(\\s[-]\\s)|\\s", "_", col)))
+        for col in irrig_coms_pd.columns]
 
+    #Merge tables
+    irrig_coms_pd['Code_dep'] = irrig_coms_pd.Code_com.str[0:2]
+    irrig_comsdepsmerge_pd = irrig_coms_pd.merge(irrig_deps_pd, on="Code_dep", how='left')
 
+    #Remove French guyana
+    irrig_comsdepsmerge_pd = irrig_comsdepsmerge_pd.loc[irrig_comsdepsmerge_pd['Code_dep'] != '97']
+
+    irrig_comsdepsmerge_pd['irrig_area_com'] =  (irrig_comsdepsmerge_pd.SAU_en_2020_com
+                                                 * irrig_comsdepsmerge_pd.Part_des_superficies_irriguées_dans_la_SAU_2020_com/100.0)
+    irrig_comsdepsmerge_pd['irrig_area_com_depsum'] = irrig_comsdepsmerge_pd.groupby('Code_dep')['irrig_area_com'].transform('sum')
+    irrig_comsdepsmerge_pd['irrig_area_dep'] = (irrig_comsdepsmerge_pd.SAU_en_2020_dep
+                                                 * irrig_comsdepsmerge_pd.Part_des_superficies_irriguées_dans_la_SAU_2020_dep/100.0)
+    #In Doubs, 0% of SAU is irrigated so won't interpolate
+    irrig_comsdepsmerge_pd.loc[(irrig_comsdepsmerge_pd.irrig_area_com_depsum > irrig_comsdepsmerge_pd.irrig_area_dep)]
+
+    irrig_comsdepsmerge_pd['irrig_area_comsumdep_diff'] = (irrig_comsdepsmerge_pd['irrig_area_dep']
+                                                           - irrig_comsdepsmerge_pd['irrig_area_com_depsum'])
+    irrig_comsdepsmerge_pd.loc[irrig_comsdepsmerge_pd.irrig_area_comsumdep_diff <0, 'irrig_area_comsumdep_diff'] = 0 #Make sure 0 for Doubs
+
+    #Interpolate area of irrigated agriculture with communes with NAs due to anonymity by allocating remaining irrigated
+    #area in the department proportionaly to the SAU (utilized agricultural surface area)
+    #If 1000 ha of irrigated area are unassigned in a department, and a commune has 10% of the SAU with NA in the department
+    #then it is assigned 10ha
+    irrig_comsdepsmerge_pd['SAU_with_NAirrig_com'] = (
+            irrig_comsdepsmerge_pd['Part_des_superficies_irriguées_dans_la_SAU_2020_com'].isna()
+            *irrig_comsdepsmerge_pd.SAU_en_2020_com)
+    irrig_comsdepsmerge_pd['SAU_with_NAirrig_depsum'] = (irrig_comsdepsmerge_pd.groupby('Code_dep')['SAU_with_NAirrig_com'].
+                                                         transform('sum'))
+
+    (irrig_comsdepsmerge_pd['irrig_area_comsumdep_diff']>irrig_comsdepsmerge_pd['SAU_with_NAirrig_depsum']).sum()
+
+    irrig_comsdepsmerge_pd['irrig_area_com_interp'] = (irrig_comsdepsmerge_pd['irrig_area_comsumdep_diff']
+                                                       *irrig_comsdepsmerge_pd['SAU_with_NAirrig_com']
+                                                       /irrig_comsdepsmerge_pd['SAU_with_NAirrig_depsum'])
+
+    irrig_comsdepsmerge_pd['irrig_area_com_all'] = (irrig_comsdepsmerge_pd.irrig_area_com.fillna(0) +
+                                                    irrig_comsdepsmerge_pd.irrig_area_com_interp.fillna(0))
+
+        #Check that numbers add up
+        # check = pd.concat(
+        #     [irrig_comsdepsmerge_pd.groupby('Code_dep')['irrig_area_com_all'].sum(),
+        #      irrig_comsdepsmerge_pd.groupby('Code_dep')['irrig_area_dep'].unique()],
+        #     axis=1
+        # )
+        # check['check'] = (check.irrig_area_com_all != check.irrig_area_dep)
+    #Export table
+    if not arcpy.Exists(irrig_coms_formatted):
+        irrig_comsdepsmerge_pd.to_csv(irrig_coms_formatted)
+
+    #Join it to the commune shapefile
+    arcpy.MakeFeatureLayer_management(coms, 'coms_lyr', where_clause="NOT (INSEE_DEP IN ('2A', '2B'))")
+    arcpy.management.AddJoin(in_layer_or_view='coms_lyr', in_field='INSEE_COM',
+                             join_table=irrig_coms_formatted, join_field='Code_com')
+    arcpy.CopyFeatures_management('coms_lyr', irrig_coms_poly)
+
+#--------------------------------- Create barrier points --------------------------------------------------------------
+if not arcpy.Exists(barriers_pts_proj):
+    barriers_pts = os.path.join(pregdb, 'barriers_amber_wgs84')
+    arcpy.management.XYTableToPoint(in_table=barriers, out_feature_class=barriers_pts,
+                                    x_field='Longitude_WGS84', y_field='Latitude_WGS84',
+                                    coordinate_system=arcpy.SpatialReference(4326))
+    arcpy.management.Project(barriers_pts, barriers_pts_proj, out_coor_system=sr_template)
+    arcpy.Delete_management(barriers_pts)
+
+#--------------------------------- Create fish station points  ---------------------------------------------------------
+if not arcpy.Exists(fish_stations_pts):
+    arcpy.management.XYTableToPoint(in_table=fish_stations, out_feature_class=fish_stations_pts,
+                                    x_field='sta_coordonnees_x', y_field='sta_coordonnees_y',
+                                    coordinate_system=sr_template)
+
+#--------------------------------- Create hydrobiology station points  ---------------------------------------------------------
+hydrobio_stations_pts = os.path.join(pregdb, "hydrobio_stations_naiade")
+
+if not arcpy.Exists(hydrobio_stations_pts):
+    hydrobio_stations_copy = os.path.join(resdir, "hydrobio_stations_nariade_copy.csv")
+    pd.read_csv(hydrobio_stations, sep=";", encoding='cp1252').to_csv(hydrobio_stations_copy, encoding='utf-8', sep=";")
+    arcpy.management.XYTableToPoint(in_table=hydrobio_stations_copy, out_feature_class=hydrobio_stations_pts,
+                                    x_field='CoordXStationMesureEauxSurface', y_field='CoordYStationMesureEauxSurface',
+                                    coordinate_system=sr_template)
+    arcpy.Delete_management(hydrobio_stations_copy)
+
+#--------------------------------- Create water withdrawal points  -----------------------------------------------------
+if not arcpy.Exists(withdrawal_pts_proj):
+    withdrawal_pts = os.path.join(pregdb, 'withdrawal_bnpe_wgs84')
+    arcpy.management.XYTableToPoint(in_table=withdrawal_stations_bnpe, out_feature_class=withdrawal_pts,
+                                    x_field='longitude', y_field='latitude',
+                                    coordinate_system=arcpy.SpatialReference(4326))
+    arcpy.management.Project(withdrawal_pts, withdrawal_pts_proj, out_coor_system=sr_template)
+    arcpy.Delete_management(withdrawal_pts)
+
+#--------------------------------- Create ONDE points  ---------------------------------------------------------
+onde_stations_pts = os.path.join(pregdb, "onde_stations")
+
+if not arcpy.Exists(onde_stations_pts):
+    onde_cat = os.path.join(resdir, "onde_obs_all.csv")
+    pd.concat(
+        [pd.read_file(onde_yr) for onde_yr in onde_filelist]
+    ).to_file(onde_cat)
+
+    arcpy.management.XYTableToPoint(in_table=onde_stations_copy, out_feature_class=onde_stations_pts,
+                                    x_field='Coordonnée X', y_field='Coordonnée Y',
+                                    coordinate_system=sr_template)
+    arcpy.Delete_management(onde_cat)
+
+#--------------------------------- Format INSEE population data  -------------------------------------------------------
+#Filter and merge buildings
+if not arcpy.Exists(buildings_filtered_fr):
+    buildings_filelist = getfilelist(bdtopo2019_dir, "BATIMENT.shp$")
+    #buildings_shp = buildings_filelist[0]
+
+    temp_buildings_list= []
+    for buildings_shp in buildings_filelist:
+        temp_lyr=os.path.join(tempgdb,
+                              "{}_batiments".format(
+                                  re.sub('[-]', '_',
+                                         Path(buildings_shp).parts[-3])
+                              ))
+        if not arcpy.Exists(temp_lyr):
+            print("Processing {}".format(os.path.split(temp_lyr)[1]))
+
+            arcpy.MakeFeatureLayer_management(in_features=buildings_shp,
+                                   out_layer='buildings_lyr',
+                                   where_clause=("((USAGE1 IN ('Résidentiel', 'Indifférencié')) "
+                                                 "OR (USAGE2 IN ('Résidentiel', 'Indifférencié'))) "
+                                                 "AND (NATURE IN ('Indifférenciée', 'Château')) "
+                                                 "AND (LEGER = 'Non') "
+                                                 "AND (ETAT='En service') "
+                                                 "AND ((HAUTEUR > 2) OR (HAUTEUR = 0))"))
+            arcpy.CopyFeatures_management(in_features='buildings_lyr',
+                                          out_feature_class=temp_lyr
+                                          )
+            temp_buildings_list.append(temp_lyr)
+            arcpy.AddGeometryAttributes_management(temp_lyr, Geometry_Properties='AREA', Area_Unit='SQUARE_METERS')
+            with arcpy.da.UpdateCursor(temp_lyr, ['POLY_AREA']) as cursor:
+                for row in cursor:
+                    if row[0] < 20:
+                        cursor.deleteRow()
+            arcpy.Delete_management('buildings_lyr')
+
+    print("Merging all building layers...")
+    arcpy.Merge_management(inputs=temp_buildings_list, output=buildings_filtered_fr)
+    print("Deleting temporary building layers...")
+    for temp_lyr in temp_buildings_list:
+        arcpy.Delete_management(temp_lyr)
+
+#Intersect buildings with variable mesh size
+
+#For residential buildings without # of households, assign based on height
+
+#For indifférencié buildings, assign 1 household?
+
+#Apportion population in variable mesh census based on number of households in each building
+
+#Check for outliers
+
+#Spatial join buildings to 200-m mesh
+
+#Compute total population in each 200-m mesh
+
+#COnvert 200-m mesh to raster
 
 
